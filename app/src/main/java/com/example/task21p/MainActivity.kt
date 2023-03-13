c3d908aa73d1e97685839d75bb4e45e6826876bc
lst97 <laisiotou1997@gmail.com>
2023-03-13 17:14:38 +1100

    Implemented requirement

--- app/src/main/java/com/example/task21p/MainActivity.kt
+++ app/src/main/java/com/example/task21p/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.task21p
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputField = findViewById<EditText>(R.id.textbox)
+        val submitBtn = findViewById<Button>(R.id.button)
+        val label = findViewById<TextView>(R.id.textView)
+
+        submitBtn.setOnClickListener{
+            val enteredValue = inputField.text.toString();
+            val message = "Welcome to SIT305 $enteredValue!"
+            label.text = message;
+        }
     }
+
+
 }
